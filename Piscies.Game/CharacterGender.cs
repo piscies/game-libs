a60142210590ca6b2608b40bf567513fdba6bd8c
Marco <maruko.sensei@gmail.com>
2019-05-03 15:44:22 -0300

    First commit with dice, names and genders

---
+++ Piscies.Game/CharacterGender.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Piscies.Game
+{
+    public class CharacterGender
+    {
+        public enum Gender
+        {
+            Male = 1,
+            Female = 2
+        }
+
+        private Gender genderValue;
+
+        public CharacterGender(Gender gender)
+        {
+            genderValue = gender;
+        }
+
+        #region Comparison Methods
+        public bool IsFemale()
+        {
+            if (genderValue == Gender.Female)
+                return true;
+            return false;
+        }
+        public bool IsMale()
+        {
+            if (genderValue == Gender.Male)
+                return true;
+            return false;
+        }
+        #endregion
+
+        #region Pronouns
+        /// <summary>
+        /// his or her
+        /// </summary>
+        public string PosessivePronoum
+        {
+            get
+            {
+                if (genderValue == Gender.Male)
+                    return "his";
+                if (genderValue == Gender.Female)
+                    return "her";
+                return "";
+            }
+        }
+        /// <summary>
+        /// His or Her
+        /// </summary>
+        public string PosessivePronoumUpper
+        {
+            get
+            {
+                if (genderValue == Gender.Male)
+                    return "His";
+                if (genderValue == Gender.Female)
+                    return "Her";
+                return "";
+            }
+        }
+        /// <summary>
+        /// him or her
+        /// </summary>
+        public string ObjectivePronoum
+        {
+            get
+            {
+                if (genderValue == Gender.Male)
+                    return "him";
+                if (genderValue == Gender.Female)
+                    return "her";
+                return "";
+            }
+        }
+        /// <summary>
+        /// Him or Her
+        /// </summary>
+        public string ObjectivePronoumUpper
+        {
+            get
+            {
+                if (genderValue == Gender.Male)
+                    return "Him";
+                if (genderValue == Gender.Female)
+                    return "Her";
+                return "";
+            }
+        }
+        /// <summary>
+        /// he or she
+        /// </summary>
+        public string SubjectivePronoum
+        {
+            get
+            {
+                if (genderValue == Gender.Male)
+                    return "he";
+                if (genderValue == Gender.Female)
+                    return "she";
+                return "";
+            }
+        }
+        /// <summary>
+        /// He or She
+        /// </summary>
+        public string SubjectivePronoumUpper
+        {
+            get
+            {
+                if (genderValue == Gender.Male)
+                    return "He";
+                if (genderValue == Gender.Female)
+                    return "She";
+                return "";
+            }
+        }
+        #endregion
+    }
+}
---
+++ Piscies.Game/Die.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Piscies.Game
+{
+    public static class Die
+    {
+        private static Random randomGenerator = new Random(Environment.TickCount);
+
+        /// <summary>
+        /// Rolls a die. Min value is always 1.
+        /// </summary>
+        /// <param name="nSides">Number of sides of the die. This is the INCLUSIVE maxValue.</param>
+        /// <returns></returns>
+        public static int Roll(int nSides)
+        {
+            return randomGenerator.Next(1, nSides + 1);
+        }
+
+        /// <summary>
+        /// Rolls a random number using Random class.
+        /// </summary>
+        /// <param name="minValue">INCLUSIVE min value.</param>
+        /// <param name="maxValue">INCLUSIVE max value</param>
+        /// <returns></returns>
+        public static int RandomNumber(int minValue, int maxValue)
+        {
+            return randomGenerator.Next(minValue, maxValue + 1);
+        }
+    }
+}
